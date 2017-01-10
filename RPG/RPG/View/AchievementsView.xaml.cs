de3a185542132a4f5847dabf43507ed612770f02
朱宇辰 <zhuyc110@qq.com>
2017-01-10 22:20:02 +0800

    Push missed file

---
+++ RPG/RPG/View/AchievementsView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using RPG.ViewModel;
+using System.ComponentModel.Composition;
+using RPG.Infrastructure.Interfaces;
+
+namespace RPG.View
+{
+    /// <summary>
+    /// AchievementsView.xaml 的交互逻辑
+    /// </summary>
+    [Export(typeof(AchievementsView))]
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    public partial class AchievementsView : IView<AchievementsViewModel>
+    {
+        public AchievementsView()
+        {
+            InitializeComponent();
+        }
+
+        [Import(typeof(AchievementsViewModel))]
+        public AchievementsViewModel ViewModel
+        {
+            set { DataContext = value; }
+        }
+    }
+}
