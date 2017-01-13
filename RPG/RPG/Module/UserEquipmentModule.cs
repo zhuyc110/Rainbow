d0cd22a0e69f4f9fafb80eed1ee2eed0cb7f6b6b
朱宇辰 <zhuyc110@qq.com>
2017-01-13 21:17:16 +0800

    Update the chapter view

---
+++ RPG/RPG/Module/UserEquipmentModule.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Composition;
+using Prism.Mef.Modularity;
+using Prism.Modularity;
+using Prism.Regions;
+using RPG.View;
+
+namespace RPG.Module
+{
+    [ModuleExport(typeof(UserEquipmentModule))]
+    public class UserEquipmentModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        [ImportingConstructor]
+        public UserEquipmentModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion(nameof(UserEquipmentModule), typeof(UserEquipmentView));
+        }
+    }
+}
