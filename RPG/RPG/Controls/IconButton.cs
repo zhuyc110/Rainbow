4f061c76ab9f467f9d1ecb55689575832c9619be
朱宇辰 <zhuyc110@qq.com>
2017-01-23 22:18:25 +0800

    Update the IconButton

---
+++ RPG/RPG/Controls/IconButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RPG.Controls
+{
+    public class IconButton : Button
+    {
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register(nameof(ImageSource), typeof(ImageSource), typeof(IconButton),
+                new PropertyMetadata(default(ImageSource)));
+
+        public static readonly DependencyProperty ContextRowProperty =
+            DependencyProperty.Register(nameof(ContextRow), typeof(int), typeof(IconButton),
+                new PropertyMetadata(default(int)));
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource) GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        public int ContextRow
+        {
+            get { return (int)GetValue(ContextRowProperty); }
+            set { SetValue(ContextRowProperty, value); }
+        }
+    }
+}
