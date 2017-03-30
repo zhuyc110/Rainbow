b9964941a544e5e770e13c531e0394deea256437
朱宇辰 <zhuyc110@qq.com>
2017-03-30 21:04:55 +0800

    Add basic localization.

---
+++ RPG/RPG/Resource/Localization/StringDefinitions.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+// ReSharper disable InconsistentNaming
+
+namespace RPG.Resource.Localization
+{
+    public static class StringDefinitions
+    {
+        public static string MainView_BlackMarket => UiTexts.ResourceManager.GetString(nameof(MainView_BlackMarket), CultureInfo.CurrentCulture);
+    }
+}
